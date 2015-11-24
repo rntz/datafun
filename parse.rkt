d394c7d93113f53ea9f1586322724540c89ecfe8
Michael Arntzenius <daekharel@gmail.com>
2015-11-24 15:08:23 +0000

    add beginnings of declaration parsing

--- parse.rkt
+++ parse.rkt
@@ -4,7 +4,8 @@
 (provide (all-defined-out))
 
 ;; a simple s-expression syntax for the language.
-;; context are simple lists of identifiers.
+;; contexts Γ are simple lists of identifiers.
+
 (define (parse-expr e Γ)
   ;; (printf "(parse-expr ~v ~v)\n" e Γ)
   (define (r e) (parse-expr e Γ))
@@ -61,3 +62,53 @@
     [(or `(tag ,name ,pat) `(',name ,pat))
       (p-tag name (parse-pat pat))]
     [_ (error "unfamiliar pattern:" p)]))
+
+
+;;; Declaration/definition parsing
+(struct defn (name type expr) #:transparent)
+
+;; decls are used internally to produce defns, so we can separate type
+;; annotations from declarations. we don't parse the expressions or types until
+;; we turn them into defns.
+(enum decl
+  (d-ann name type)
+  (d-def name expr))
+
+;; produces a list of defns. for now, we don't support referring to a variable
+;; before its definition, not even if you give it a type-signature. also, all
+;; recursion must be explicit via `fix'.
+(define (parse-decls ds Γ)
+  (decls->defns (append* (map parse-decl ds)) Γ))
+
+;; produces a list of decls.
+(define (parse-decl d)
+  ;; (printf "(parse-decl ~v ~v)\n" d Γ)
+  (match d
+    [`(: ,(? symbol? name) ,t) (list (d-ann name t))]
+    [`(def ,(? symbol? name) ,e) (list (d-def name e))]
+    [`(def ,(? symbol? name) ,t ,e)
+      (list (d-ann name t) (d-def name e))]
+    [`(def (,(? symbol? name) ,(? symbol? args) ...) ,body)
+      ;; this is a crude hack
+      (list (d-def name `(λ ,@args ,body)))]
+    [`(def (,(? symbol? name) ,(? symbol? args) ...) ,t ,body)
+      (list
+        (d-ann name t)
+        ;; same hack here
+        (d-def name `(λ ,@args ,body)))]))
+
+;; list of decls -> list of defns
+(define (decls->defns ds Γ)
+  (define acc '())
+  (define type-sigs (make-hash))
+  (for ([d ds])
+    (match d
+      [(d-ann n t) (hash-set! type-sigs n (parse-type t))]
+      [(d-def n e)
+        (define t (hash-ref type-sigs n #f))
+        (unless t (error "no type signature for:" n))
+        (set! e (parse-expr e Γ))
+        (set! Γ (cons n Γ))
+        (set! acc (cons (defn n t e) acc))
+        (hash-remove! type-sigs n)]))
+  (reverse acc))
