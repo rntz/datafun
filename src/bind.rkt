e1f75922f296e11754fbe92e98f7977fc14e2c01
Michael Arntzenius <daekharel@gmail.com>
2015-12-22 17:16:39 -0500

    remove aborted work on abt views

--- src/bind.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(require "util.rkt" "ast.rkt")
-(provide (all-defined-out))
-
-;; Takes an expr in which all variables are free and then binds them according
-;; to their names.
-(define (bind e))
