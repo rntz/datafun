c371fb818beb9ea3c5495d35848280010b854a2f
Michael Arntzenius <daekharel@gmail.com>
2016-02-15 19:35:52 +0000

    more work on parser

--- src/parser.rkt
+++ src/parser.rkt
@@ -23,34 +23,39 @@
    LPAREN RPAREN LSQUARE RSQUARE LCURLY RCURLY
    COMMA BAR : SEMI = =>
    ;; declarations
-   TYPE
+   TYPE FUN VAL
    ;; types
    * + SET MAP
    ;; patterns
    WILD
    ;; expressions
-   LAMBDA LET FIX IN CASE IF THEN ELSE
+   LAMBDA LET FIX CASE IF THEN ELSE
+   IN ∈ FOR
    TRUE FALSE UNIT))
-
-;; TODO: lub-comprehensions
 
 
 ;; ========== LEXER ==========
 (define datafun-lex
   (lexer-src-pos
+   ;; whitespace & comments
+   [(:+ whitespace) (return-without-pos (datafun-lex input-port))]
+   ;; we don't support nested comments yet, alas.
+   [(:: "(*" (complement (:: any-string "*)" any-string)) "*)")
+     (return-without-pos (datafun-lex input-port))]
+
    ;; punctuation
    ["(" 'LPAREN]  [")" 'RPAREN] ["{" 'LCURLY]  ["}" 'RCURLY]
    ["[" 'LSQUARE] ["]" 'RSQUARE]
    ["=>" '=>] ["," 'COMMA] ["|" 'BAR] [":" ':] [";" 'SEMI] ["=" '=]
 
    ;; declarations
-   ["type" 'TYPE]
+   ["type" 'TYPE] ["val" 'VAL] ["fun" 'FUN]
    ["const" (token-tone 'any)]
    ["mono"  (token-tone 'mono)]
    ["anti"  (token-tone 'anti)]
 
    ;; types
-   ["*" '*] ["+" '+] ["Set" 'SET] ["Map" 'MAP]
+   ["*" '*] ["+" '+] ["set" 'SET] ["map" 'MAP]
    ["->"              (token-arrow 'any)]
    [(:or "~>" "->+")  (token-arrow 'mono)]
    ["->-"             (token-arrow 'anti)]
@@ -61,13 +66,18 @@
    ;; expressions
    [(:or "fn" "λ") 'LAMBDA] ["let" 'LET] ["fix" 'FIX] ["in" 'IN] ["case" 'CASE]
    ["if" 'IF] ["then" 'THEN] ["else" 'ELSE]
-   [(:or "in?" "<=" "<" ">=" ">") (token-binop (string->symbol lexeme))]
-   [(:or "∨" "lub") (token-binop 'lub)]
+   [(:or "<=" "<" ">=" ">") (token-binop (string->symbol lexeme))]
+   [(:or "in?" "∈?") (token-binop 'in?)]
+   [(:or "∨" "lub")  (token-binop 'lub)]
    [(:or "empty" "ε") 'UNIT]
-   ;; literals
+   [(:or "for" "⋁")   'FOR]
+   ["∈" '∈]
+
+   ;; literals.
    ["true" 'TRUE] ["false" 'FALSE]
    [(:+ numeric) (token-number (string->number lexeme))]
-   ;; TODO: string literals
+   [(:: "\"" (:* (:or (:~ "\"") (:: "\\" any-char))) "\"")
+    (token-string (with-input-from-string lexeme read))]
 
    ;; identifiers
    [(:: (:& alphabetic lower-case)
@@ -75,10 +85,7 @@
     (token-id (string->symbol lexeme))]
    [(:: (:& alphabetic upper-case)
         (:* (:or alphabetic numeric (char-set "-_"))))
-    (token-Id (string->symbol lexeme))]
-
-   ;; whitespace & comments
-   [(:+ whitespace) (return-without-pos (datafun-lex input-port))]))
+    (token-Id (string->symbol lexeme))]))
 
 
 ;; ========== GRAMMAR / PARSER ==========
@@ -97,13 +104,20 @@
    ;; s-expressions that parse.rkt can parse.
    (grammar
 
-    (decl ((TYPE id = type) (list `(type ,$2 ,$4)))
-          ((id : type)      (list `(: ,$1 ,$3)))
-          ((id args = expr) (list `(= ,$1 ,$2 ,$4))))
+    (decls (() '())
+           ((decl decls) (append $1 $2)))
+    (decl ((TYPE id = type)             `((type ,$2 ,$4)))
+          ((tone? VAL id : type)        `((,@$1 ,$3 : ,$5)))
+          ((tone? VAL id = expr)        `((,@$1 ,$3 = ,$5)))
+          ((tone? VAL id : type = expr) `((,@$1 ,$3 : ,$5) (,$3 = ,$7)))
+          ((tone? FUN id args = expr)   `((,@$1 ,$3 ,@$4 = ,$6))))
     (args (() '())
           ((id args) (cons $1 $2)))
 
-    ;; TODO: sum/variant types, record types
+    (tone? (()     '())
+           ((tone) (list $1)))
+
+    ;; TODO: record types
     (type ((func-type) $1))
     (func-type ((arg-type) $1)
                ((arg-type arrow func-type)
@@ -113,13 +127,26 @@
     (factor-types ((factor-type * factor-types) (cons $1 $3))
                   ((factor-type) (list $1)))
     (factor-type
+     ((atom-type)               $1)
+     ((summand-types1)          (cons '+ $1))
+     ((SET atom-type)           `(set ,$2))
+     ((MAP atom-type atom-type) `(map ,$2 ,$3)))
+    (atom-type
      ((id)                     $1)
-     ((LPAREN type RPAREN)     $2)
-     ((SET type)               `(set ,$2))
-     ((MAP type type)          `(map ,$2 ,$3)))
+     ((LPAREN RPAREN)          '(*))
+     ((LPAREN type RPAREN)     $2))
+    (summand-types1
+     ((summand-type)                    (list $1))
+     ((summand-type + summand-types1)   (cons $1 $3)))
+    (summand-type ((Id) `',$1)
+                  ((Id LPAREN comma-types RPAREN) `(',$1 ,@$3)))
+    (comma-types (()                        '())
+                 ((type)                    `(,$1))
+                 ((type COMMA comma-types)  (cons $1 $3)))
 
     ;; literals
     (lit ((number)      $1)
+         ((string)      $1)
          ((TRUE)        '#t)
          ((FALSE)       '#f))
 
@@ -129,45 +156,66 @@
           ((WILD)                       '_)
           ((= expr)                     `(= ,$2))
           ((LPAREN pat RPAREN)          $2)
-          ((LPAREN comma-pats RPAREN)   `(cons ,@$2))
+          ((LPAREN comma-pats* RPAREN)  `(cons ,@$2))
+          ((Id)                         `',$1)
           ((Id LPAREN comma-pats RPAREN)    (match $3
                                               ['()  `',$1]
-                                              [pats `(',$1 ,@pats)]))
-          ((Id)                             `',$1))
-    (comma-pats (() '())
-                ((pat) (list $1))
-                ((pat COMMA comma-pats) (cons $1 $3)))
+                                              [pats `(',$1 ,@pats)])))
+    (comma-pats* (() '())
+                 ((pat COMMA comma-pats) (cons $1 $3)))
+    (comma-pats ((comma-pats*) $1)
+                ((pat)         (list $1)))
 
     ;; expressions
-    (expr ((LPAREN expr RPAREN) $2)
-          ((LPAREN comma-exprs RPAREN) `(cons ,@$2))
-          ((id)                 $1)
-          ((lit)                $1)
-          ((UNIT)               'empty)
-          ((expr binop expr)    `(,$2 ,$1 ,$3)) ;woo prefix notation
-          ((expr * expr)        `(* ,$1 ,$3))
-          ((expr + expr)        `(+ ,$1 ,$3))
-          ((expr = expr)        `(= ,$1 ,$3))
-          ((expr : type)        `(as ,$3 ,$1))
-          ;; TODO: let-expressions
-          ((LAMBDA args => expr)            `(fn ,@$2 ,$4))
-          ((IF expr THEN expr ELSE expr)    `(if ,$2 ,$4 ,$6))
-          ((CASE expr cases)                `(case ,$2 ,@$3))
-          ((FIX id = expr)                  `(fix ,$2 ,$4))
-          ((FIX id : type = expr)           `(as ,$4 (fix ,$2 ,$6)))
-          ((LCURLY comma-exprs RCURLY)      `(set ,@$2))
-          ((LCURLY expr BAR loops RCURLY)   `((set ,$2) ,@$4)))
-    (comma-exprs (() '())
-                 ((expr) (list $1))
-                 ((expr COMMA comma-exprs) (cons $1 $3)))
+    (expr ((LAMBDA args => expr)        `(fn ,@$2 ,$4))
+          ((LET decls IN expr)          `(let ,$2 ,$4))
+          ((CASE op-expr cases)         `(case ,$2 ,@$3))
+          ((IF op-expr THEN op-expr ELSE op-expr)    `(if ,$2 ,$4 ,$6))
+          ((FIX id = expr)              `(fix ,$2 ,$4))
+          ((FIX id : type = expr)       `(as ,$4 (fix ,$2 ,$6)))
+          ((FOR LPAREN loops RPAREN expr) `(,@$3 lub ,$5))
+          ((asc-expr)                    $1))
+    (asc-expr ((asc-expr : type)    `(as ,$3 ,$1))
+              ((op-expr)            $1))
+    (op-expr
+     ((app-expr binop app-expr)     `(,$2 ,$1 ,$3)) ;woo prefix notation
+     ((app-expr * app-expr)         `(* ,$1 ,$3))
+     ((app-expr + app-expr)         `(+ ,$1 ,$3))
+     ((app-expr = app-expr)         `(= ,$1 ,$3))
+     ((app-expr)                    $1))
+    (app-expr
+     ((app-expr arg-expr)   `(,$1 ,$2))
+     ((arg-expr)            $1))
+    (arg-expr
+     ((id)                              $1)
+     ((lit)                             $1)
+     ((UNIT)                            'empty)
+     ((LPAREN expr RPAREN)              $2)
+     ((LPAREN comma-exprs* RPAREN)      `(cons ,@$2))
+     ((LCURLY comma-exprs RCURLY)       `(set ,@$2))
+     ((LCURLY op-expr BAR loops RCURLY) `((set ,$2) ,@$4))
+     ;; TODO: multiple-element map literals; map comprehensions?
+     ((LCURLY : RCURLY)                 `(map))
+     ((LCURLY kv-exprs1 RCURLY)         `(map ,@$2)))
+
+    (kv-exprs1 ((kv-expr) (list $1))
+               ((kv-expr COMMA kv-exprs1) (cons $1 $3)))
+    (kv-expr   ((op-expr : op-expr) (list $1 $3)))
+
+    (comma-exprs ((comma-exprs*) $1)
+                 ((op-expr) (list $1)))
+    (comma-exprs* (() '())
+                  ((op-expr COMMA comma-exprs) (cons $1 $3)))
+
     (cases (() '())
            ((case cases) (cons $1 $2)))
     (case  ((BAR pat => expr) (list $2 $4)))
     (loops ((loop) $1)
            ((loop COMMA loops) (append $1 $3)))
     ;; TODO: looping across a map.
-    (loop  ((expr)         `(when ,$1))
-           ((expr IN expr) `(for ,$1 in ,$3)))
+    (loop  ((op-expr)         `(when ,$1))
+           ((pat IN op-expr)  `(for ,$1 in ,$3))
+           ((pat ∈ op-expr)   `(for ,$1 in ,$3)))
 
     )))
 
