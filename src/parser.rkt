8e306a40e9ba18145deb0a63b0c57e399d3eba9b
Michael Arntzenius <daekharel@gmail.com>
2016-02-16 13:44:40 +0000

    parser.rkt: annotate everything

--- src/parser.rkt
+++ src/parser.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require "util.rkt" "ast.rkt" "debug.rkt" "parse.rkt")
-(provide source-info parse-port parse-string parse-file)
+(provide source-info get-source-info parse-port parse-string parse-file)
 
 (require parser-tools/lex parser-tools/yacc)
 (require (prefix-in : parser-tools/lex-sre))
@@ -87,10 +87,22 @@
 ;; it will bind each expression, pat, and decl to (list start-pos end-pos),
 ;; where start-pos & end-pos are position structures.
 (define source-info (make-parameter (make-weak-hasheq)))
-(define (annotate! obj start-pos end-pos)
+
+(define (get-source-info e)
+  (and (source-info) (hash-ref source-info e #f)))
+
+(define (annotate-with! obj start-pos end-pos)
   (when (source-info)
     (hash-set! (source-info) obj (list start-pos end-pos)))
   obj)
+
+;; magic variable-capturing macro
+(define-syntax-parser annotate!
+  [(_ e ctx) (with-syntax ([start-pos (datum->syntax #'ctx '$1-start-pos)]
+                           [end-pos   (datum->syntax #'ctx '$n-end-pos)])
+               #'(annotate-with! e start-pos end-pos))]
+  [(_ e) #'(annotate! e e)]
+  [self:id #'(lambda (e) (annotate! e self))])
 
 (define/match (parse-func how)
   [('expr)  expr-parse]
@@ -126,63 +138,61 @@
     (decls
      (() '())
      ((decl decls) (append $1 $2)))
-    (decl ((d) (annotate! $1 $1-start-pos $1-end-pos)))
+    (decl ((d) (map annotate! $1)))
     (d
      ((TYPE name = type)            `((type ,$2 = ,$4)))
      ((tone? VAL name-list1 : type) `((,@$1 ,@$3 : ,$5)))
-     ;; gah. this is conflicting with the previous rule somehow.
-     ;; for example, (parse "val x : nat" #:as 'decls) errors.
-     ;((tone? VAL name : type = expr) `((,@$1 ,$3 : ,$5) (,$3 = ,$7)))
      ((tone? VAL name = expr)       `((,@$1 ,$3 = ,$5)))
      ((tone? FIX name = expr)       `((,@$1 fix ,$3 = ,$5)))
-     ((tone? FUN name names1 = expr)            `((,@$1 ,$3 ,@$4 = ,$6)))
-     ((tone? FUN LP name oper name RP = expr)   `((,@$1 ,$5 ,$4 ,$6 = ,$9))))
+     ((tone? FUN name names1 = expr) `((,@$1 ,$3 ,@$4 = ,$6)))
+     ((tone? FUN LP name oper name RP = expr) `((,@$1 ,$5 ,$4 ,$6 = ,$9))))
 
     (tone? (() '()) ((tone) (list $1)))
     (tone ((CONST) 'const) ((MONO) 'mono) ((ANTI) 'anti))
 
     ;; ----- types -----
     ;; TODO: record types
-    (type ((t) (annotate! $1 $1-start-pos $1-end-pos)))
-    (t
+    (type
      ((t-func) $1)
-     ((t-sum)  `(+ ,@$1)))
+     ((t-sum)  (annotate! `(+ ,@$1))))
     (t-func
-     ((t-arg) $1)
-     ((t-arg -> t-func) `(,$1 -> ,$3))
-     ((t-arg ~> t-func) `(,$1 ~> ,$3))
-     ((t-arg ->+ t-func) `(,$1 ->+ ,$3))
-     ((t-arg ->- t-func) `(,$1 ->- ,$3)))
+     ((t-arg)            $1)
+     ((t-arg -> t-func)  (annotate! `(,$1 -> ,$3)))
+     ((t-arg ~> t-func)  (annotate! `(,$1 ~> ,$3)))
+     ((t-arg ->+ t-func) (annotate! `(,$1 ->+ ,$3)))
+     ((t-arg ->- t-func) (annotate! `(,$1 ->- ,$3))))
     (t-arg
      ((t-product) (match $1
                     [(list x) x]
-                    [xs `(* ,@xs)])))
+                    [xs (annotate! `(* ,@xs))])))
     (t-product
      ((t-factor)              (list $1))
      ((t-factor * t-product)  (cons $1 $3)))
     (t-factor
      ((t-atom)                      $1)
-     ((SET t-atom)                  `(set ,$2))
-     ((MAP t-atom t-atom)           `(map ,$2 ,$3))
-     ((LCURLY type RCURLY)          `(set ,$2))
-     ((LCURLY t-list+ RCURLY)       `(set (* ,@$2)))
-     ((LCURLY type : type RCURLY)   `(map ,$2 ,$4)))
-    (t-atom
-     ((id)          $1)
-     ((LP RP)       '(*))
-     ((LP type RP)  $2))
+     ((SET t-atom)                  (annotate! `(set ,$2)))
+     ((MAP t-atom t-atom)           (annotate! `(map ,$2 ,$3))))
+
     (t-sum
      ((t-ctor)           (list $1))
      ((t-ctor BAR t-sum) (cons $1 $3)))
     (t-ctor
-     ((Id) $1)
+     ((Id)              $1)
      ((Id LP t-list RP) `(,$1 ,@$3)))
+
+    (t-atom
+     ((id)              $1)
+     ((LP t-big RP)     $2)
+     ((LCURLY t-big RCURLY)         (annotate! `(set ,$2)))
+     ((LCURLY type : t-big RCURLY)  (annotate! `(map ,$2 ,$4))))
+    (t-big                              ; a type inside parentheses or similar
+     (()    '(*))
+     ((type) $1)
+     ((type COMMA t-list) (annotate! `(* ,$1 ,@$3))))
     (t-list
-     (()                        '())
-     ((type)                    `(,$1))
+     (()                   '())
+     ((type)               `(,$1))
      ((type COMMA t-list)  (cons $1 $3)))
-    (t-list+
-     ((type COMMA t-list) (cons $1 $3)))
 
     ;; ----- literals -----
     (lit ((number)      $1)
@@ -191,62 +201,61 @@
          ((FALSE)       '#f))
 
     ;; ----- patterns -----
-    (pat ((p) (annotate! $1 $1-start-pos $1-end-pos)))
-    (p
+    (pat
      ((name)            $1)
      ((lit)             $1)
-     ((_)               '_)
-     ((= expr)          `(= ,$2))
+     ((_)               '_)             ;can't annotate a symbol, alas
+     ((= expr)          (annotate! `(= ,$2)))
      ((LP pat RP)       $2)
-     ((LP p-list* RP)   `(cons ,@$2))
-     ((Id)              `',$1)
-     ((Id LP p-list RP) (match $3
-                          ['()  `',$1]
-                          [pats `(',$1 ,@pats)])))
+     ((LP p-list* RP)   (annotate! `(cons ,@$2)))
+     ((Id)              (annotate! `',$1))
+     ((Id LP p-list RP) (annotate! (match $3
+                                     ['()  `',$1]
+                                     [pats `(',$1 ,@pats)]))))
     (p-list* (() '())
              ((pat COMMA p-list) (cons $1 $3)))
     (p-list ((p-list*) $1)
             ((pat)         (list $1)))
 
     ;; ----- expressions -----
-    (expr ((e) (annotate! $1 $1-start-pos $1-end-pos)))
+    (expr ((e) (annotate! $1)))
     (e
-     ((LAMBDA names => expr)        `(fn ,@$2 ,$4))
-     ((LET decls IN expr)           `(let ,$2 ,$4))
-     ((CASE e-op cases)             `(case ,$2 ,@$3))
-     ((IF e-op THEN e-op ELSE e-op) `(if ,$2 ,$4 ,$6))
-     ((WHEN e-op THEN expr)         `(when ,$2 ,$4))
-     ((UNLESS e-op THEN expr)       `(unless ,$2 ,$4))
-     ((FIX name = expr)             `(fix ,$2 ,$4))
-     ((FIX name : type = expr)      `(as ,$4 (fix ,$2 ,$6)))
-     ((⋁ LP loops RP expr)          `(,@$3 lub ,$5))
-     ((FOR LP loops RP expr)        `(,@$3 lub ,$5))
+     ((LAMBDA names => expr)        (annotate! `(fn ,@$2 ,$4)))
+     ((LET decls IN expr)           (annotate! `(let ,$2 ,$4)))
+     ((CASE e-op cases)             (annotate! `(case ,$2 ,@$3)))
+     ((IF e-op THEN e-op ELSE e-op) (annotate! `(if ,$2 ,$4 ,$6)))
+     ((WHEN e-op THEN expr)         (annotate! `(when ,$2 ,$4)))
+     ((UNLESS e-op THEN expr)       (annotate! `(unless ,$2 ,$4)))
+     ((FIX name = expr)             (annotate! `(fix ,$2 ,$4)))
+     ((FIX name : type = expr)      (annotate! `(as ,$4 (fix ,$2 ,$6))))
+     ((⋁ LP loops RP expr)          (annotate! `(,@$3 lub ,$5)))
+     ((FOR LP loops RP expr)        (annotate! `(,@$3 lub ,$5)))
      ((e-asc)                       $1))
     (e-asc
-     ((e-asc : type)    `(as ,$3 ,$1))
+     ((e-asc : type)    (annotate! `(as ,$3 ,$1)))
      ((e-op)            $1))
     (e-op
      ;; for now, everything is left associative. TODO: precedence parsing.
-     ((e-op oper e-app)      `(,$2 ,$1 ,$3))
-     ((e-op IN? e-app)       `(,$1 in? ,$3))
-     ((Id LP e-list RP)      `(',$1 ,@$3))
-     ((Id)                   `',$1)
+     ((e-op oper e-app)      (annotate! `(,$2 ,$1 ,$3)))
+     ((e-op IN? e-app)       (annotate! `(,$1 in? ,$3)))
+     ((Id LP e-list RP)      (annotate! `(',$1 ,@$3)))
+     ((Id)                   (annotate! `',$1))
      ((e-app)                $1))
     (e-app
-     ((e-app e-atom)   `(,$1 ,$2))
+     ((e-app e-atom)   (annotate! `(,$1 ,$2)))
      ((e-atom)         $1))
     (e-atom
      ((name)                            $1)
      ((lit)                             $1)
      ((EMPTY)                           'empty)
      ((LP expr RP)                      $2)
-     ((LP expr BAR loops RP)            `(,$2 ,@$4))
-     ((LP e-list* RP)                   `(cons ,@$2))
-     ((LCURLY e-list RCURLY)            `(set ,@$2))
-     ((LCURLY e-op BAR loops RCURLY)    `((set ,$2) ,@$4))
+     ((LP expr BAR loops RP)            (annotate! `(,$2 ,@$4)))
+     ((LP e-list* RP)                   (annotate! `(cons ,@$2)))
+     ((LCURLY e-list RCURLY)            (annotate! `(set ,@$2)))
+     ((LCURLY e-op BAR loops RCURLY)    (annotate! `((set ,$2) ,@$4)))
      ;; TODO: multiple-element map literals; map comprehensions?
-     ((LCURLY : RCURLY)                 `(map))
-     ((LCURLY e-kv-list1 RCURLY)        `(map ,@$2)))
+     ((LCURLY : RCURLY)                 (annotate! `(map)))
+     ((LCURLY e-kv-list1 RCURLY)        (annotate! `(map ,@$2))))
 
     (oper ((=) '=) ((<=) '<=) ((>=) '>=) ((<) '<) ((>) '>)
           ((++) '++) ((+) '+) ((-) '-) ((*) '*) ((/) '/)
@@ -295,7 +304,7 @@
       (parse-port port #:as how))))
 
 
-;; TESTING
+;; ;; TESTING
 ;; (define parse parse-string)
 
 ;; (define (tokenize s)
