9c0b403d63cc90dccd1a83497a40361547df3830
Michael Arntzenius <daekharel@gmail.com>
2018-02-22 16:36:23 -0500

    temporary work

--- v3-web/src/ExampleFinal.ml
+++ v3-web/src/ExampleFinal.ml
@@ -1,156 +1,224 @@
-open Sigs
-open Util
-
-type loc = Lexing.position * Lexing.position
-type var = string
-type tag = string
-
-type tp = Base | Fn of tp * tp | Sum of (tag * tp) list
-
-module Type = struct
-  let rec join (a: tp) (b: tp): tp = if a = b then a else todo()
-  let rec meet (a: tp) (b: tp): tp = if a = b then a else todo()
-  let rec subtype (a: tp) (b: tp): bool = a = b || todo()
-
-  exception NoEquality of tp
-  exception Incompatible of tp * tp
-  type equal = [`Base | `Sum of (tag * equal) list]
-  let rec equal: tp -> equal = function
-    | Base -> `Base
-    | Sum pts -> `Sum (List.map (fun (n,t) -> (n,equal t)) pts)
-    | Fn _ as t -> raise (NoEquality t)
-end
-
-module type FRONT = sig
-  type pat
-  type exp
-
-  module Pat: sig
-    val var: var -> pat
-    val tag: tag -> pat -> pat
-  end
-
-  module Exp: sig
-    val ann: loc -> exp -> exp
-    val var: var -> exp
-    val the: tp -> exp -> exp
-    val lam: pat -> exp -> exp
-    val app: exp -> exp -> exp
-    val tag: tag -> exp -> exp
-    val case: exp -> (pat * exp) list -> exp
-  end
-end
-
-module type MIDDLE = sig
-  type pat
-  type exp
-
-  module Pat: sig
-    val var: var -> pat
-    val tag: tag -> pat -> pat
-    val eq: Type.equal -> exp -> pat
-  end
-
-  module Exp: sig
-    val var: var -> exp
-    val lam: pat -> exp -> exp
-    val tag: tag -> exp -> exp
-    val app: exp -> exp -> exp
-    val case: exp -> (pat * exp) list -> exp
-  end
-end
-
-
-(* Frontend functor - does typechecking/elaboration *)
-module Front(Mid: MIDDLE) = struct
-  (* types are syntactic *)
-  type cx = (var * tp) list
-
-  (* patterns & expressions are semantic *)
-  type pat = cx -> tp -> cx * Mid.pat
-  type exp = cx -> tp option -> tp * Mid.exp
-
-  exception Fail of string
-  exception FailAt of loc * string
-  let fail msg = raise (Fail msg)
-
-  module Pat = struct
-    module P = Mid.Pat
-    let var v cx tp = match List.assoc_opt v cx with
-      | None -> ((v,tp)::cx, P.var v)
-      (* We can compare x:A with y:B if (A ⊔ B) has equality. I think
-       * heterogenous equality, which would work if (A ⊓ B) supports equality,
-       * is possible. However, it complicates the type of (=).
-       *
-       * Ideally we'd like to issue an error here if (A ⊓ B) is empty.
-       * For now we omit this.
-       *)
-      | Some xtp ->
-         try (cx, P.eq Type.(equal (join tp xtp)) (Mid.Exp.var v))
-         with Type.Incompatible _ -> fail "incompatible types"
-            | Type.NoEquality _ -> fail "type does not support equality tests"
-
-    let tag (n:tag) (p:pat) (cx:cx) (tp:tp) = match tp with
-      | Sum nts ->
-         let (cx, p) = (try p cx (List.assoc n nts)
-                        with Not_found -> fail "tag not in sum type")
-         in (cx, P.tag n p)
-      | _ -> fail "tag pattern must have sum type"
-  end
-
-  module Exp = struct
-    module E = Mid.Exp
-
-    let synthOnly ((got, exp): (tp * Mid.exp)) (expect: tp option): tp * Mid.exp =
-      match expect with
-      | None -> (got, exp)
-      | Some want -> if Type.subtype got want
-                     then (want, exp)
-                     else fail "type mismatch"
-
-    let checkOnly (a: tp -> Mid.exp) (expect: tp option): tp * Mid.exp =
-      match expect with
-      | None -> fail "cannot infer"
-      | Some want -> (want, a want)
-
-    let ann loc e cx expect =
-      try e cx expect
-      with Fail why -> raise (FailAt (loc,why))
-
-    let var v cx = synthOnly
-      (try (List.assoc v cx, E.var v)
-       with Not_found -> fail "unbound variable")
-
-    let the tp e cx = synthOnly (e cx (Some tp))
-
-    let lam (p:pat) (e:exp) cx = checkOnly **> function
-      | Fn(a,b) -> let (cx, p) = p cx a in
-                   let (_, e) = e cx (Some b) in
-                   E.lam p e
-      | _ -> fail "lambdas are functions"
-
-    let app x y cx = synthOnly **>
-      let (a,b,xe) = match x cx None with
-        | Fn(a,b), exp -> (a,b,exp)
-        | _ -> fail "applying non-function" in
-      let (_,ye) = y cx (Some a) in
-      (b, E.app xe ye)
-
-    let tag (n: tag) (e: exp) cx (expect: tp option) =
-      let get_type = function
-        | Sum nts -> (try List.assoc n nts
-                      with Not_found -> fail "tag not found")
-        | _ -> fail "not a sum type" in
-      let (tp,e) = e cx Option.(expect => get_type) in
-      (Option.default (Sum [n,tp]) expect, E.tag n e)
-
-    (* for now, we can only check case expressions *)
-    let case (subj:exp) arms cx = checkOnly **> fun tp ->
-      let (subjt, subje) = subj cx None in
-      let doArm (pat, exp) =
-        let (cx,pat) = pat cx subjt in
-        let (_,exp) = exp cx (Some tp) in
-        (pat,exp) in
-      E.case subje (List.map doArm arms)
-  end
-end
+(* open Sigs
+ * open Util
+ * 
+ * type loc = Lexing.position * Lexing.position
+ * type var = string
+ * type tag = string
+ * 
+ * 
+ * (\* ----- Tones ----- *\)
+ * type tone = Id | Op | Iso | Path
+ * module Tone = struct
+ *   let compose (a: tone) (b: tone): tone = match a,b with
+ *     | x, Id | Id, x -> x
+ *     | Op, Op -> Id
+ *     | _, Iso | _, Path -> b   (\* hi priority, must come first *\)
+ *     | Iso, _ | Path, _ -> a   (\* lo priority, comes later *\)
+ *   let ( * ) = compose
+ * 
+ *   let meet (a: tone) (b: tone): tone = match a, b with
+ *     | Iso, _ | _, Iso | Id, Op | Op, Id -> Iso
+ *     | x, Path | Path, x -> x
+ *     | Id, Id | Op, Op -> a
+ * 
+ *   let (<=) (a: tone) (b: tone): bool = match a,b with
+ *     | Iso, _ | _, Path -> true
+ *     | Id, Id | Op, Op -> true
+ *     | _, Iso | Path, _ | Op, Id | Id, Op -> false
+ * end
+ * 
+ * module type TONES = sig
+ *   type t
+ *   val mkEmpty: unit -> t
+ *   val use: t -> var -> tone -> unit
+ *   (\* push binds a variable *\)
+ *   val push: t -> var -> unit
+ *   (\* pop unbinds a variable, returning its tone of use *\)
+ *   val pop: t -> var -> tone
+ * end
+ * 
+ * module Tones: TONES = struct
+ *   open Hashtbl
+ *   type t = (var, tone) Hashtbl.t
+ *   let mkEmpty () = create ~random:true 10
+ *   let use tbl v how = find tbl v |> Tone.meet how |> replace tbl v
+ *   let push tbl v = add tbl v Path
+ *   let pop tbl v = let tone = find tbl v in remove tbl v; tone
+ * end
+ * 
+ * 
+ * (\* ----- Types ----- *\)
+ * type tp = Base | Fn of tp * tp | Sum of (tag * tp) list
+ * module Type = struct
+ *   let rec join (a: tp) (b: tp): tp = if a = b then a else todo()
+ *   let rec meet (a: tp) (b: tp): tp = if a = b then a else todo()
+ *   let rec (<=) (a: tp) (b: tp): bool = a = b || todo()
+ * 
+ *   exception NoEquality of tp
+ *   exception Incompatible of tp * tp
+ *   type equal = [`Base | `Sum of (tag * equal) list]
+ *   let rec equal: tp -> equal = function
+ *     | Base -> `Base
+ *     | Sum pts -> `Sum (List.map (fun (n,t) -> (n,equal t)) pts)
+ *     | Fn _ as t -> raise (NoEquality t)
+ * end
+ * 
+ * 
+ * (\* ----- Signatures ----- *\)
+ * module type FRONT = sig
+ *   type pat
+ *   type exp
+ * 
+ *   module Pat: sig
+ *     (\* problem: automatic unboxing. argh!
+ *      * wants to be type-directed! *\)
+ *     val var: var -> pat
+ *     val tag: tag -> pat -> pat
+ *     (\* TODO: *\)
+ *     (\* val box: pat -> pat *\)
+ *   end
+ * 
+ *   module Exp: sig
+ *     val ann: loc -> exp -> exp
+ *     val var: var -> exp
+ *     val the: tp -> exp -> exp
+ *     val lam: pat -> exp -> exp
+ *     val app: exp -> exp -> exp
+ *     val tag: tag -> exp -> exp
+ *     val case: exp -> (pat * exp) list -> exp
+ *   end
+ * end
+ * 
+ * module type MIDDLE = sig
+ *   type pat
+ *   type exp
+ * 
+ *   module Pat: sig
+ *     val var: var -> pat
+ *     val tag: tag -> pat -> pat
+ *     val eq: Type.equal -> exp -> pat
+ *   end
+ * 
+ *   module Exp: sig
+ *     val var: var -> exp
+ *     val lam: pat -> exp -> exp
+ *     val tag: tag -> exp -> exp
+ *     val app: exp -> exp -> exp
+ *     val case: exp -> (pat * exp) list -> exp
+ *   end
+ * end
+ * 
+ * 
+ * (\* ----- Typechecking/elaboration ----- *\)
+ * module Front(Mid: MIDDLE) = struct
+ *   exception Fail of string
+ *   exception FailAt of loc * string
+ *   let fail msg = raise (Fail msg)
+ * 
+ *   (\* pattern & expression semantics *\)
+ *   type cx = (var * tp) list
+ *   type tones = Tones.t
+ *   (\* bugger. *\)
+ *   (\* type pat = <run: 'a.
+ *    *                  cx -> tones -> tp ->
+ *    *                  (\\* continuation-passing! *\\)
+ *    *                  (cx -> tones -> 'a) -> 'a > *\)
+ *   (\* won't this work? *\)
+ *   type pat = cx -> tones -> tp -> Mid.pat * (var * tone) list * cx
+ * 
+ *   type exp = cx -> tones -> tp option -> tp * Mid.exp
+ * 
+ *   module Pat = struct
+ *     module P = Mid.Pat
+ *     let var v cx tones tp = match List.assoc_opt v cx with
+ *       (\* PROBLEM: need to BIND a variable in tones
+ *        * - yet somehow communicate what tone it's bound at
+ *        * to the outside!
+ *        *
+ *        * we really do want an update monad here, I think.
+ *        * ok, so inline it.
+ *        *\)
+ *       | None -> (P.var v, [v,Id], (v,tp)::cx)
+ *       (\* We can compare x:A with y:B if (A ⊔ B) has equality. I think
+ *        * heterogenous equality, which would work if (A ⊓ B) supports equality,
+ *        * is possible. However, it complicates the type of (=).
+ *        *
+ *        * Ideally we'd like to issue an error here if (A ⊓ B) is empty.
+ *        * For now we omit this.
+ *        *\)
+ *       | Some vtp ->
+ *          Tones.use tones v Iso;
+ *          let equal =
+ *            try Type.(equal (join tp vtp))
+ *            with Type.NoEquality _ -> fail "type does not support equality tests"
+ *               | Type.Incompatible _ -> fail "incompatible types" in
+ *          (P.eq equal (Mid.Exp.var v), [], cx)
+ * 
+ *     let tag (n:tag) (p:pat) (cx:cx) tones (tp:tp) = match tp with
+ *       | Sum nts ->
+ *          let (p, binds, cx) = (try p cx tones (List.assoc n nts)
+ *                                with Not_found -> fail "tag not in sum type")
+ *          in (P.tag n p, binds, cx)
+ *       | _ -> fail "tag pattern must have sum type"
+ *   end
+ * 
+ *   module Exp = struct
+ *     module E = Mid.Exp
+ * 
+ *     (\* type exp = cx -> tones -> tp option -> tp * Mid.exp *\)
+ * 
+ *     let infers ((got, exp): (tp * Mid.exp)) (expect: tp option): tp * Mid.exp =
+ *       match expect with
+ *       | None -> (got, exp)
+ *       | Some want when Type.(got <= want) -> (want,exp)
+ *       | Some want -> fail "type mismatch"
+ * 
+ *     let checks (a: tp -> Mid.exp) (expect: tp option): tp * Mid.exp =
+ *       match expect with
+ *       | None -> fail "cannot infer"
+ *       | Some want -> (want, a want)
+ * 
+ *     let ann (loc:loc) (e:exp) cx tones expect =
+ *       try e cx tones expect
+ *       with Fail why -> raise (FailAt (loc,why))
+ * 
+ *     let var v cx tones =
+ *       let tp = try List.assoc v cx
+ *                with Not_found -> fail "unbound variable" in
+ *       Tones.use tones v Id;
+ *       infers (tp, E.var v)
+ * 
+ *     let the tp e cx tones = infers (e cx tones (Some tp))
+ * 
+ *     (\* PROBLEM: need to bind a variable in tones! *\)
+ *     let lam (p:pat) (e:exp) cx tones = checks **> function
+ *       | Fn(a,b) -> let (cx, p) = p cx tones a in
+ *                    let (_, e) = e cx (Some b) in
+ *                    E.lam p e
+ *       | _ -> fail "lambdas are functions"
+ * 
+ *     let app x y cx = infers **>
+ *       let (a,b,xe) = match x cx None with
+ *         | Fn(a,b), exp -> (a,b,exp)
+ *         | _ -> fail "applying non-function" in
+ *       let (_,ye) = y cx (Some a) in
+ *       (b, E.app xe ye)
+ * 
+ *     let tag (n: tag) (e: exp) cx (expect: tp option) =
+ *       let get_type = function
+ *         | Sum nts -> (try List.assoc n nts
+ *                       with Not_found -> fail "tag not found")
+ *         | _ -> fail "not a sum type" in
+ *       let (tp,e) = e cx Option.(expect => get_type) in
+ *       (Option.default (Sum [n,tp]) expect, E.tag n e)
+ * 
+ *     (\* for now, we can only check case expressions *\)
+ *     let case (subj:exp) arms cx = checks **> fun tp ->
+ *       let (subjt, subje) = subj cx None in
+ *       let doArm (pat, exp) =
+ *         let (cx,pat) = pat cx subjt in
+ *         let (_,exp) = exp cx (Some tp) in
+ *         (pat,exp) in
+ *       E.case subje (List.map doArm arms)
+ *   end
+ * end *)
--- v3-web/src/ExampleInitial.ml
+++ v3-web/src/ExampleInitial.ml
@@ -10,13 +10,59 @@ type loc = Lexing.position * Lexing.position
 type var = string
 type tag = string
 
+             
+(* ----- Tones ----- *)
+type tone = Id | Op | Iso | Path
+module Tone = struct
+  (* TODO: comment about order of composition *)
+  let compose (a: tone) (b: tone): tone = match a,b with
+    | x, Id | Id, x -> x
+    | Op, Op -> Id
+    | Iso, _ | Path, _ -> a   (* hi priority, must come first *)
+    | _, Iso | _, Path -> b   (* lo priority, comes later *)
+  let ( * ) = compose
+
+  let meet (a: tone) (b: tone): tone = match a, b with
+    | Iso, _ | _, Iso | Id, Op | Op, Id -> Iso
+    | x, Path | Path, x -> x
+    | Id, Id | Op, Op -> a
+
+  let (<=) (a: tone) (b: tone): bool = match a,b with
+    | Iso, _ | _, Path -> true
+    | Id, Id | Op, Op -> true
+    | _, Iso | Path, _ | Op, Id | Id, Op -> false
+end
+
+module type TONES = sig
+  type t
+  val mkEmpty: unit -> t
+  val use: t -> var -> tone -> unit
+  (* push binds a variable *)
+  val push: t -> var -> unit
+  (* pop unbinds a variable, returning its tone of use *)
+  val pop: t -> var -> tone
+end
+
+module Tones: TONES = struct
+  open Hashtbl
+  type t = (var, tone) Hashtbl.t
+  let mkEmpty () = create ~random:true 10
+  let use tbl v how = find tbl v |> Tone.meet how |> replace tbl v
+  let push tbl v = add tbl v Path
+  let pop tbl v = let tone = find tbl v in remove tbl v; tone
+end
+
+
+(* ----- Types, patterns, expressions ----- *)
 type tp =
   [ `Base
+  | `Box of tp
   | `Fn of tp * tp
   | `Sum of (string * tp) list ]
 
 type pat =
   [ `Var of var
+  | `Box of pat
   | `Tag of string * pat ]
 
 type 'a expF =
@@ -37,23 +83,23 @@ module Type = struct
   let join (a: tp) (b: tp): tp = if a = b then a else todo()
   let meet (a: tp) (b: tp): tp = if a = b then a else todo()
   let rec subtype (a: tp) (b: tp): bool = a = b || todo()
+
+  exception NoEquality of tp
+  type equal = [ `Base | `Sum of (tag * equal) list ]
+  let rec equal: tp -> equal = function
+    | `Base -> `Base
+    | `Sum pts -> `Sum (List.map (fun (n,t) -> (n,equal t)) pts)
+    | `Fn _ as t -> raise (NoEquality t)
+    | `Box p -> equal p
 end
 
 
 (* Intermediate language, after type checking. *)
 module IL = struct
-  type equal = [ `Base | `Sum of (tag * equal) list ]
-
-  exception NoEquality of tp
-  let rec equal: tp -> equal = function
-    | `Base -> `Base
-    | `Sum pts -> `Sum (List.map (fun (n,t) -> (n,equal t)) pts)
-    | `Fn _ as t -> raise (NoEquality t)
-
   type pat =
     [ `Var of var
     | `Tag of tag * pat
-    | `Eq of equal * exp ]
+    | `Eq of Type.equal * exp ]
   and exp =
     [ `Var of var
     | `Lam of pat * exp
@@ -63,140 +109,177 @@ module IL = struct
 end
 
 
-(* The typechecker monad *)
+(* ----- Typechecking ----- *)
 type cx = (var * tp) list
-type 'a fail = Ok of 'a | Fail of (expr * string) list
-(* Maybe don't use a monad at all?
- * threading context through isn't hard
- * failure can use exceptions
- * what about writing tones?
- * can pass & thread through a hashtable.
- *
- * TODO: investigate this.
- *)
-type 'a infer = cx -> 'a fail
-
-module Infer = struct
-  let (>>=) a f cx = match a cx with Ok x -> f x cx | Fail msgs -> Fail msgs
-  let concat a = a >>= id
-
-  include Idiom(struct
-    type 'a t = 'a infer
-    let pure x cx = Ok x
-    let map f a = a >>= f @> pure
-    let ( ** ) a b cx = match a cx, b cx with
-      | Ok x, Ok y -> Ok (x,y)
-      | Fail xs, Fail ys -> Fail (xs @ ys)
-      | Fail msgs, Ok _ | Ok _, Fail msgs -> Fail msgs
-  end)
-
-  let getContext cx = Ok cx
-  let withContext cx a _ = a cx
-  let fail locmsg cx = Fail [locmsg]
-  let withVar (var, tp) a cx = a ((var,tp)::cx)
-end
-
-(* pattern inference raises exceptions on failure. *)
-type 'a patInfer = cx -> cx * 'a
-module PatInfer = struct
-  include Monad(struct
-    type 'a t = 'a patInfer
-    let pure x cx = (cx, x)
-    let map f a cx = let (cx,x) = a cx in (cx, f x)
-    let (>>=) a f cx = let (cx,x) = a cx in f x cx
-  end)
-end
-
-
-(* Typechecking *)
 type expect = tp option
+type tones = Tones.t
+exception Fail of loc * string
 
-(* hold on, which exceptions can this generate?
- * - Type.Incompatible
- * - IL.NoEquality
+(* if I ever add disjunction/conjunction patterns, modifying cx
+ * in place will cause me trouble; I'll need to rewrite this.
  *
- * argh, and WHEN does it generate them?
- * answer: either after (elabPat pat tp) or after (elabPat pat tp cx).
+ * do I really need to have both patTones & useTones???
  *)
-let rec elabPat: pat -> tp -> IL.pat patInfer = fun pat tp ->
-  let open PatInfer in
-  match pat, tp with
-  | `Var x, tp ->
-     (fun cx -> match List.assoc_opt x cx with
-                | None -> ((x,tp)::cx, `Var x)
-                (* We can compare x:A with y:B if (A ⊔ B) has equality.
-                 * I think heterogenous equality, which would work if (A ⊓ B)
-                 * supports equality, is possible. However, it complicates the
-                 * type of (=).
-                 *
-                 * Ideally we'd like to issue an error here if (A ⊓ B) is empty.
-                 * For now we omit this.
-                 *)
-                | Some xtp -> (cx, `Eq (IL.equal (Type.join tp xtp), `Var x)))
+let rec elabPat: loc -> cx ref -> tones -> tones -> tone -> tp -> pat
+                 -> IL.pat =
+  fun loc cx patTones useTones tone tp pat ->
+  let recurAt = elabPat loc cx patTones useTones in
+  let recur = recurAt tone in
+  let fail msg = raise (Fail (loc,msg)) in
+  match tp, pat with
+  (* explicit/automatic unboxing *)
+  | `Box a, `Box p | `Box a, p ->
+  (* Path because elimination (Box a -> a) is Path-toned.
+   * TODO: think about composition order here! *)
+     recurAt Tone.(tone * Path) a p
 
-  | `Tag(n,p), `Sum nts ->
+  | _, `Box p -> fail "matching box pattern against non-box type"
+
+  | tp, `Var v ->
+     begin match List.assoc_opt v !cx with
+     | None ->
+        Tones.push useTones v;
+        Tones.push patTones v; Tones.use patTones v tone;
+        cx := (v,tp) :: !cx;
+        `Var v
+     | Some vtp ->
+        Tones.use patTones v tone;
+        Tones.use useTones v Iso;
+        `Eq (Type.(equal (join tp vtp)), `Var v)
+     end
+
+  | `Sum nts, `Tag (n,p) ->
      let tp = try List.assoc n nts
-              with Not_found -> todo() in
-     elabPat p tp => C.tag n
-  (* TODO: exception *)
-  | `Tag(n,p), _ -> raise (todo())
+              with Not_found -> fail "tag not in sum type" in
+     `Tag(n, recur tp p)
 
-let rec elab: expr -> expect -> (tp * IL.exp) infer =
-  fun (loc,expr) expect ->
-  let open Infer in
-  let fail x = fail ((loc,expr), x) in
-  let check tp e = elab e (Some tp) => snd in
-  let inferPat p tp =
-    getContext >>= fun cx ->
-    try pure (elabPat p tp cx)
-    with Type.Incompatible _ -> fail "incompatible types"
-       | IL.NoEquality _ -> fail "comparing at non-equality type" in
+  | _, `Tag _ -> fail "tag must have sum type"
 
-  let withPat p tp body =
-    inferPat p tp >>= fun (cx, p) -> withContext cx (body p) in
-
-  let checkOnly (f: tp -> IL.exp infer): (tp * IL.exp) infer =
+let rec elab: cx -> tones -> expect -> expr -> tp * IL.exp =
+  fun cx tones expect (loc,expr) ->
+  let fail msg = raise (Fail (loc,msg)) in
+  let synthesize (got, e) =
     match expect with
-    | Some tp -> pure tp ** f tp
-    | None -> fail "cannot infer type" in
-  let synthed (tp, exp) = match expect with
-    | None -> pure (tp, exp)
-    | Some expected ->
-       if Type.subtype tp expected then pure (expected, exp)
-       else fail "not a subtype" in
+    | None -> (got, e)
+    | Some want when Type.(got <= want) -> (want, e)
+    | Some want -> fail "inferred type not a subtype of expected type" in
+  let needType () = match expect with
+    | None -> fail "I can't infer the type here; could you annotate it for me?"
+    | Some tp -> tp in
 
-  getContext >>= fun cx ->
+  let checking f = let t = needType() in (t, f t) in
+
+  let infer = elab cx tones None in
+  let check tp = elab cx tones (Some tp) in
+
   match expr with
-  | `Var v -> (try pure (List.assoc v cx, `Var v)
-               with Not_found -> fail "unbound variable")
-              >>= synthed
+  | `Var v ->
+     let tp = try List.assoc v cx with Not_found -> fail "unbound variable" in
+     Tones.use tones v Id;
+     synthesize (tp, `Var v)
 
-  | `The (tp,e) -> elab e (Some tp) >>= synthed
+  | `The(a,x) -> synthesize (check a x)
 
-  | `Lam (p,e) ->
-     checkOnly
-     **> fun tp -> (match tp with `Fn x -> pure x
-                                | _ -> fail "lambdas have function type")
-     >>= fun (a,b) -> withPat p a (fun p -> C.lam p $ check b e)
+  | `Lam(p,x) -> todo()
 
-  | `App (e1,e2) ->
-     elab e1 None >>= (function `Fn(a,b), e1e -> pure (a,b,e1e)
-                              | _ -> fail "applying non-function")
-     >>= fun (a, b, e1e) -> check a e2
-     >>= fun e2e -> synthed (b, `App(e1e,e2e))
+  | `App(x,y) ->
+     let (a,b,xe) = match infer x with
+       | `Fn(a,b), xe -> a,b,xe
+       | _ -> fail "applying non-function" in
+     let (_, ye) = check a y in
+     synthesize (b, `App(xe,ye))
 
-  | `Tag (n,e) ->
+  | `Tag(n,x) ->
      let get_type = function
-       | `Sum nts -> (try pure (List.assoc n nts)
+       | `Sum nts -> (try List.assoc n nts
                       with Not_found -> fail "tag not found")
        | _ -> fail "not a sum type" in
-     Infer.option Option.(map get_type expect)
-     >>= fun e_expect -> elab e e_expect
-     => fun (tp, exp) -> (Option.default (`Sum [n,tp]) expect, `Tag (n,exp))
+     let (xt,xe) = elab cx tones Option.(expect => get_type) x in
+     (Option.default (`Sum [n,xt]) expect, `Tag(n,xe))
 
-  | `Case (subj, arms) -> checkOnly **> fun tp ->
-     (* for now, we only can _check_ case expressions *)
-     elab subj None >>= fun (subjt, subje) ->
-     map (fun arms -> `Case(subje, arms))
-     **> forEach arms
-     **> fun (pat,exp) -> withPat pat subjt (fun p -> pure p ** check tp exp)
+  | `Case(subj, arms) ->
+     (* for now, we can only check case expressions. *)
+     checking **> fun tp ->
+     (* we need to capture the way `subj` uses variables, and modify that
+      * according to how the pattern & body use `subj`.
+      * so we create an empty Tones.t to pass in. *)
+     let subjTones = Tones.mkEmpty() in
+     let (subjt, subje) = elab cx subjTones None subj in
+     let doArm (pat, exp) =
+       (* Likewise, we capture how `pat` & `exp` use variables. *)
+       let useTones = Tones.mkEmpty () in
+       (* patTones is the "mediating" tones. TODO: explain better. *)
+       let (patTones, bodyCx, pat) = elabPat loc cx useTones Id subjt pat in
+       let (_, exp) = elab bodyCx useTones (Some tp) exp in
+       (* We pop all the variables that pat bound & pushed onto newTones.
+        * This tells us how (pat => exp) uses them.
+        * We compose these with patTones and take the meet, then compose
+        * the result onto the remaining tones in newTones.
+        *
+        * This tells us how the overall case-expression uses the variables that
+        * `subj` uses.
+        *)
+       todo()
+     in
+     (* this isn't gonna work, we need to extract some information, probably *)
+     todo(); `Case (subje, List.map doArm arms)
+                   
+
+(* let rec elab: cx -> tones -> expect -> (tp * IL.exp) =
+ *   fun cx tones expect (loc,expr) ->
+ *   let fail msg = raise (Fail (loc, msg)) in
+ *   let check tp e = elab e (Some tp) => snd in
+ *   let inferPat p tp =
+ *     getContext >>= fun cx ->
+ *     try pure (elabPat p tp cx)
+ *     with Type.Incompatible _ -> fail "incompatible types"
+ *        | Type.NoEquality _ -> fail "comparing at non-equality type" in
+ * 
+ *   let withPat p tp body =
+ *     inferPat p tp >>= fun (cx, p) -> withContext cx (body p) in
+ * 
+ *   let checkOnly (f: tp -> IL.exp infer): (tp * IL.exp) infer =
+ *     match expect with
+ *     | Some tp -> pure tp ** f tp
+ *     | None -> fail "cannot infer type" in
+ *   let synthed (tp, exp) = match expect with
+ *     | None -> pure (tp, exp)
+ *     | Some expected ->
+ *        if Type.subtype tp expected then pure (expected, exp)
+ *        else fail "not a subtype" in
+ * 
+ *   getContext >>= fun cx ->
+ *   match expr with
+ *   | `Var v -> (try pure (List.assoc v cx, `Var v)
+ *                with Not_found -> fail "unbound variable")
+ *               >>= synthed
+ * 
+ *   | `The (tp,e) -> elab e (Some tp) >>= synthed
+ * 
+ *   | `Lam (p,e) ->
+ *      checkOnly
+ *      **> fun tp -> (match tp with `Fn x -> pure x
+ *                                 | _ -> fail "lambdas have function type")
+ *      >>= fun (a,b) -> withPat p a (fun p -> C.lam p $ check b e)
+ * 
+ *   | `App (e1,e2) ->
+ *      elab e1 None >>= (function `Fn(a,b), e1e -> pure (a,b,e1e)
+ *                               | _ -> fail "applying non-function")
+ *      >>= fun (a, b, e1e) -> check a e2
+ *      >>= fun e2e -> synthed (b, `App(e1e,e2e))
+ * 
+ *   | `Tag (n,e) ->
+ *      let get_type = function
+ *        | `Sum nts -> (try pure (List.assoc n nts)
+ *                       with Not_found -> fail "tag not found")
+ *        | _ -> fail "not a sum type" in
+ *      Infer.option Option.(map get_type expect)
+ *      >>= fun e_expect -> elab e e_expect
+ *      => fun (tp, exp) -> (Option.default (`Sum [n,tp]) expect, `Tag (n,exp))
+ * 
+ *   | `Case (subj, arms) -> checkOnly **> fun tp ->
+ *      (\* for now, we only can _check_ case expressions *\)
+ *      elab subj None >>= fun (subjt, subje) ->
+ *      map (fun arms -> `Case(subje, arms))
+ *      **> forEach arms
+ *      **> fun (pat,exp) -> withPat pat subjt (fun p -> pure p ** check tp exp) *)
