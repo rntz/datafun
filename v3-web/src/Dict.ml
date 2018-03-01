c8c664b3c3506df3ad6e58e11d3093c65f8749ae
Michael Arntzenius <daekharel@gmail.com>
2018-02-28 20:46:09 -0500

    minor change

--- v3-web/src/Dict.ml
+++ v3-web/src/Dict.ml
@@ -1,3 +1,2 @@
 include Map.Make(String)
-let of_list (assocs: (string * 'a) list): 'a t =
-  List.fold_left (fun map (k,v) -> add k v map) empty assocs
+let of_list assocs = List.fold_left (fun map (k,v) -> add k v map) empty assocs
