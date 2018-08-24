023eb664025da23e345fe68de55b0d03de5ff7a4
Michael Arntzenius <daekharel@gmail.com>
2018-08-24 13:47:30 +0100

    makeDeps.sh: ignore commented-out lines

--- agda/makeDeps.sh
+++ agda/makeDeps.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
 set -e
 echo "digraph {"
 
 # Generate the node names.
-find . -iname '*.agda' '!' -name '.*' |
-    sed -Ee 's,^\./,,;s,.agda$,,;s,/,.,g;s/.*/"&" [label="&"];/'
-
-# Generate edges.
 find * -iname '*.agda' '!' -name '.*' |
-    xargs egrep -o 'import [[:alnum:].]+' |
-    sed -Ee 's/(.*).agda:import (.*)/"\1" -> "\2";/;s,/,.,g'
+    sed -Ee 's/.agda$//;s,/,.,g;s/.*/"&" [label="&"];/'
+
+# Generate edges. Assumes at most one import per line, but ignores commented-out
+# lines.
+find * -iname '*.agda' '!' -name '.*' |
+    xargs egrep 'import [[:alnum:].]+' |
+    grep -v -- '--.*import' |
+    sed -Ee 's/(.*).agda:.*import ([[:alnum:].]+).*/"\1" -> "\2";/;s,/,.,g'
 
 echo "}"
