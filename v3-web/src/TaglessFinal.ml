feb26c037662ab393898124fc08131932f992caa
Michael Arntzenius <daekharel@gmail.com>
2018-10-18 18:03:59 +0100

    TaglessFinal.ml: use Map.Make

--- v3-web/src/TaglessFinal.ml
+++ v3-web/src/TaglessFinal.ml
@@ -30,10 +30,9 @@ let next_id = ref 0
 let nextId() = let x = !next_id in next_id := x + 1; x
 let gensym name () = {name = name; id = nextId()}
 
-(* Contexts mapping variables to stuff. TODO: better representation. *)
-type 'a cx = (sym * 'a) list
-let get (cx: 'a cx) (x: sym): 'a = List.assoc x cx
-let set (cx: 'a cx) (x: sym) (v: 'a): 'a cx = (x,v) :: cx
+(* Contexts mapping variables to stuff. *)
+module Cx = Map.Make(struct type t = sym let compare = Pervasives.compare end)
+type 'a cx = 'a Cx.t
 
 
 (* ===== LANGUAGE ALGEBRAS as ML MODULES (a la tagless final style) ===== *)
@@ -91,9 +90,9 @@ module Interp_ = struct
   type expr = env -> value
 
   let expr _ (x: expr): term = x
-  let var _ x env = get env x
+  let var _ = Cx.find
 
-  let lam _ _ x body env = Func (fun v -> body (set env x v))
+  let lam _ _ x body env = Func (fun v -> body (Cx.add x v env))
   let app _ _ fnc arg env = match fnc env, arg env with
     | Func f, a -> f a
     | _ -> raise Impossible
@@ -113,41 +112,44 @@ end = Interp_
 
 (* A simplification pass, based on intensional normalisation by evaluation.
  * Based on http://homepages.inf.ed.ac.uk/slindley/nbe/nbe-cambridge2016.pdf
+ *
+ * Goal: eliminate all higher-order subterms, assuming our input term
+ * a) has only first-order free variables (inputs)
+ * b) has a first-order type (output)
  *)
 module Simplify_(N: NORMAL) = struct
-  type value = Neut of N.expr
+  type value = Expr of N.expr
              | Func of string * (value -> value)
              | Tuple of value list
 
   let rec reify (tp: tp) (v: value): N.term = match v,tp with
-    | Neut e, _ -> N.expr tp e
+    | Expr e, _ -> N.expr tp e
     | Tuple vs, Prod tps -> N.tuple (List.map2 (fun a v -> (a, reify a v)) tps vs)
     | Func(name,f), Fn(a,b) ->
        let x = gensym name () in
-       N.lam a b x (reify b (f (Neut (N.var a x))))
+       N.lam a b x (reify b (f (Expr (N.var a x))))
     | Tuple _, _ | Func _, _ -> raise Impossible
 
-  (* TODO: better environment representation *)
   type env = value cx
   type term = env -> value
 
-  let var a x env = try get env x with Not_found -> Neut (N.var a x)
-  let lam _ _ x m env = Func (x.name, (fun v -> m (set env x v)))
+  let var a x env = try Cx.find x env with Not_found -> Expr (N.var a x)
+  let lam _ _ x m env = Func (x.name, (fun v -> m (Cx.add x v env)))
   let tuple terms env = Tuple (List.map (fun (_,t) -> t env) terms)
 
   let app a b m n env = match m env with
-    | Neut f -> Neut (N.app a b f (reify a (n env)))
+    | Expr f -> Expr (N.app a b f (reify a (n env)))
     | Func(_,f) -> f (n env)
     | Tuple _ -> raise Impossible
 
   let proj tps i m env = match m env with
-    | Neut e -> Neut (N.proj tps i e)
+    | Expr e -> Expr (N.proj tps i e)
     | Tuple xs -> List.nth xs i
     | Func _ -> raise Impossible
 
-  let letBind _ _ x expr body env = body (set env x (expr env))
+  let letBind _ _ x expr body env = body (Cx.add x (expr env) env)
 
-  let norm tp (t: term): N.term = reify tp (t [])
+  let norm tp (t: term): N.term = reify tp (t Cx.empty)
 end
 
 module Simplify(N: NORMAL): sig
@@ -175,7 +177,7 @@ module Bidi(T: TYPED): BIDI
     else typeError "not a subtype"
 
   let lam (x: sym) (m: term) (cx: tp cx): tp -> T.term = function
-    | Fn(a,b) -> T.lam a b x (m (set cx x a) b)
+    | Fn(a,b) -> T.lam a b x (m (Cx.add x a cx) b)
     | _ -> typeError "lambda must be a function"
 
   let tuple (tms: term list) (cx: tp cx): tp -> T.term = function
@@ -185,13 +187,13 @@ module Bidi(T: TYPED): BIDI
     | _ -> typeError "tuple must be a product"
 
   let letBind (x: sym) (expr: expr) (body: term) (cx: tp cx) (b: tp): T.term =
-    let a, ex = expr cx in T.letBind a b x ex (body (set cx x a) b)
+    let a, ex = expr cx in T.letBind a b x ex (body (Cx.add x a cx) b)
 
   (* ----- Synthesizing expressions ----- *)
   let asc a m cx = (a, m cx a)
 
   let var (x: sym) (cx: tp cx): tp * T.term =
-    let tp = try get cx x with Not_found -> typeError "unbound variable"
+    let tp = try Cx.find x cx with Not_found -> typeError "unbound variable"
     in tp, T.var tp x
 
   let app (fnc: expr) (arg: term) (cx: tp cx): tp * T.term =
