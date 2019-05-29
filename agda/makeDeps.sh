70238c9b63524a7a496963b09145207254b08688
Michael Arntzenius <daekharel@gmail.com>
2019-05-29 09:43:27 +0100

    agda/: move old stuff into trash/

--- agda/makeDeps.sh
+++ agda/makeDeps.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+function find-agdas() {
+    find * -iname '*.agda' '!' -name '.*' '!' -regex '.*trash/.*'
+}
+
 set -e
 echo "digraph {"
 
 # Generate the node names.
-find * -iname '*.agda' '!' -name '.*' |
+find-agdas |
     sed -Ee 's/.agda$//;s,/,.,g;s/.*/"&" [label="&"];/'
 
 # Generate edges. Assumes at most one import per line, but ignores commented-out
 # lines.
-find * -iname '*.agda' '!' -name '.*' |
+find-agdas |
     xargs egrep 'import [^ ]+' |
     grep -v -- '--.*import' |
     sed -Ee 's/^(.*).agda:.*import ([^ ]+).*/"\1" -> "\2";/;s,/,.,g'
