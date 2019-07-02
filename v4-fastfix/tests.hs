e2c3cf952cba2bd4f881b796031fc91dac89d4c9
Michael Arntzenius <daekharel@gmail.com>
2019-07-02 01:01:57 +0100

    v4-fastfix/tests.hs: use Control.Exception.evaluate

--- v4-fastfix/tests.hs
+++ v4-fastfix/tests.hs
@@ -5,6 +5,7 @@ import System.Random
 import Data.Set hiding (union, take)
 import Text.Printf
 import System.CPUTime
+import Control.Exception (evaluate)
 
 -- Transitive closure, naively.
 trans :: Ord a => Set (a,a) -> Set (a,a)
@@ -15,28 +16,6 @@ semiTrans :: Ord a => Set (a,a) -> Set (a,a)
 semiTrans edge_9 =
   semifix ((\path_8 -> (union edge_9 (forIn edge_9 (\a_2 -> (forIn path_8 (\b_3 -> (guard ((snd a_2) == (fst b_3)) (set [((fst a_2), (snd b_3))])))))))),
            (\path_8 -> (\dpath_8 -> (forIn edge_9 (\a_2 -> (forIn dpath_8 (\b_3 -> (guard ((snd a_2) == (fst b_3)) (set [((fst a_2), (snd b_3))])))))))))
-
-test :: Int -> IO ()
-test i = do
-  let n = 10 * i
-  let edges = lineG n
-  let numEdges = size edges
-  let numNodes = size $ set $ do (x,y) <- toList edges; [x,y]
-  printf "testing %i nodes %i edges\n" numNodes numEdges
-  naiveT <- time (trans edges)
-  semiT <- time (semiTrans edges)
-  printf "  naive: %6.2fs\n" naiveT
-  printf "  semi:  %6.2fs\n" semiT
-
--- This is kind of a hack, as you can tell from the type signature.
-time :: a -> IO Double
-time x = do
-  t1 <- getCPUTime
-  t2 <- seq x getCPUTime
-  return (fromIntegral (t2-t1) * 1e-12)
-
-main :: IO ()
-main = mapM_ test [2..16]
 
 
 -- Example graphs, parameterized by a size factor.
@@ -52,3 +31,27 @@ randomG :: Int -> Set (Int,Int)
 randomG n = set $ take numEdges $ pairUp $ randomRs (1,n) $ mkStdGen $ n * 2137
   where pairUp (x:y:zs) = (x,y) : pairUp zs
         numEdges = n * floor (sqrt (fromIntegral n))
+
+
+-- What a weird type signature.
+time :: a -> IO Double
+time x = do
+  t1 <- getCPUTime
+  _ <- evaluate x
+  t2 <- getCPUTime
+  return (fromIntegral (t2-t1) * 1e-12)
+
+test :: Int -> IO ()
+test i = do
+  let n = 10 * i
+  let edges = lineG n
+  let numEdges = size edges
+  let numNodes = size $ set $ do (x,y) <- toList edges; [x,y]
+  printf "testing %i nodes %i edges\n" numNodes numEdges
+  naiveT <- time (trans edges)
+  semiT <- time (semiTrans edges)
+  printf "  naive: %6.2fs\n" naiveT
+  printf "  semi:  %6.2fs\n" semiT
+
+main :: IO ()
+main = mapM_ test [2..16]
